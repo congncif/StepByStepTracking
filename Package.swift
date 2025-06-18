38a08a67c9b8e82bcd5d507169db48ffa6f43d3e
NGUYEN CHI CONG <congnc.if@gmail.com>
2025-06-18 09:18:00 +0700

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -4,32 +4,32 @@ import PackageDescription
 let package = Package(
     name: "StepByStepTracking",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
     ],
     products: [
+        // .library(
+        //     name: "StepByStepTracking",
+        //     targets: ["StepByStepTracking"]
+        // ),
         .library(
             name: "StepByStepTracking",
             targets: ["StepByStepTracking"]
         ),
-        .library(
-            name: "StepByStepTrackingCore",
-            targets: ["StepByStepTrackingCore"]
-        ),
     ],
     targets: [
         .target(
-            name: "StepByStepTrackingCore",
+            name: "StepByStepTracking",
             path: "Sources",
             exclude: [], // exclude files if needed
             sources: nil, // all .swift files under Sources
             publicHeadersPath: nil
         ),
-        .target(
-            name: "StepByStepTracking",
-            dependencies: [
-                .target(name: "StepByStepTrackingCore")
-            ]
-        )
+        // .target(
+        //     name: "StepByStepTracking",
+        //     dependencies: [
+        //         .target(name: "StepByStepTrackingCore"),
+        //     ]
+        // ),
     ],
     swiftLanguageVersions: [.v5]
 )
